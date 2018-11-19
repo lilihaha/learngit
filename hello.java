aa89d848998e8a3a7f08bfce086d3fe03d661638
wentao <1843384931@qq.com>
2018-11-19 22:17:36 +0800

    添加remove方法

--- hello.java
+++ hello.java
@@ -2,9 +2,14 @@
 //修改默认
 //添加第二次默认
 //第三次修改
-public class hello {
+public class hello 
+	private static arrayList = new ArrayList();{
 	public static void main (Streing[] args) {
 		System.out.println("Hello World");
 		System.out.println("你好， 世界");
+	}
+
+	public static void remove (int index){
+		arrayList.remove(index);
 	}
 }
